5c233fb2af4ec44a372969b8dcfb3554a097e71d
Ravenoid01 <juanda.rave147@gmail.com>
2023-10-05 00:00:26 -0500

    Hojas de estilos

---
+++ script.js
@@ -0,0 +1,61 @@
+
+//Scroll header
+const scrollLimit = 200;
+
+function fixHeader() {
+    const header = document.getElementById("myHeader");
+    if (window.scrollY >= scrollLimit) {
+        header.style.top = "-120px";
+    } else {
+        header.style.top = "0"; 
+    }
+}
+
+window.addEventListener("scroll", fixHeader);
+
+//Div oculto
+const botonesMostrarDiv = document.querySelectorAll(".mostrarDivOculto");
+
+botonesMostrarDiv.forEach((boton, index) => {
+    boton.addEventListener("click", function() {
+        const divOculto = document.getElementById(`divOculto${index + 1}`);
+        
+        if (divOculto.style.display === "block") {
+            divOculto.style.display = "none";
+        } else {
+            divOculto.style.display = "block";
+        }
+    });
+});
+
+//Cambiar boton
+const mostrarBoton = document.getElementById("mostrarBoton1");
+const mostrarBoton2 = document.getElementById("mostrarBoton2");
+
+let estadoBoton1 = false;
+let estadoBoton2 = false;
+
+mostrarBoton1.addEventListener("click", function () {
+    estadoBoton1 = !estadoBoton1;
+  
+    if (estadoBoton1) {
+      mostrarBoton1.textContent = "Mostrar menos";
+    } else {
+      mostrarBoton1.textContent = "Mostrar más";
+    }
+  });
+  
+  mostrarBoton2.addEventListener("click", function () {
+    estadoBoton2 = !estadoBoton2;
+  
+    if (estadoBoton2) {
+      mostrarBoton2.textContent = "Mostrar menos";
+    } else {
+      mostrarBoton2.textContent = "Mostrar más";
+    }
+  });
+
+
+
+
+
